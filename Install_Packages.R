14bede18c189fde6dd649fddeecad2a117b04934
snandi <nandi@stat.wisc.edu>
2015-07-19 13:51:28 -0500

    Added UsingR to InstallPackages

--- Install_Packages.R
+++ Install_Packages.R
@@ -104,7 +104,8 @@ Packages <- c(
   'svMisc', 
   'tseries', 
   'TTR', 
-  'urca', 
+  'urca',
+  'UsingR',    
   'vrtest', 
   'wle', 
   'wordcloud', 
@@ -113,7 +114,7 @@ Packages <- c(
   'xtable', 
   'xts', 
   'zipcode', 
-  'zoo', 
+  'zoo'
 )
 
 for(Package in Packages){
@@ -123,117 +124,4 @@ for(Package in Packages){
     print(paste(Package, 'already exists'))
   }
 }
-
-# install.packages('alr3')
-# install.packages('biganalytics')
-# install.packages('biglm')
-# install.packages('boot')
-# install.packages('car')
-# install.packages('chron')
-# install.packages('cluster', dependencies = TRUE)
-# install.packages('clusterSim', dependencies = TRUE) ## Wont install on lmcg
-# install.packages('clValid')
-# install.packages('DAAG')
-# install.packages('dtw')
-# install.packages('epicalc')
-# install.packages('faraway')
-# install.packages('fAssets')
-# install.packages('fBasics')
-# install.packages('fda')
-# install.packages('fields')
-# install.packages('foreach')
-# install.packages('foreign', dependencies = TRUE)
-# install.packages('fpc', dependencies = TRUE) # For cluster comparisons ## Wont install on lmcg
-# install.packages('fPortfolio', dependencies = TRUE)
-# install.packages('fRegression')
-# install.packages('fSeries')
-# install.packages('fUtilities')
-# install.packages('gam')
-# install.packages('gdata')
-# install.packages('glmnet')
-# install.packages('gmaps')
-# install.packages('gmodels')
-# install.packages('ggplot2') ## Wont install on lmcg
-# install.packages('gplots')
-# install.packages('graphics')
-# install.packages('gridExtra')
-# install.packages('gsubfn')
-# install.packages('gtools')
-# install.packages('HiddenMarkov')
-# install.packages('HMM')
-# install.packages('Hmisc')
-# install.packages('hydroTSM')
-# install.packages('inline')
-# install.packages('iterators')
-# install.packages('kernlab')
-# install.packages('knitr')
-# install.packages('leaps')
-# install.packages('limma')
-# install.packages('lme4')
-# install.packages('lmtest')
-# install.packages('lpSolve')
-# install.packages('lubridate')
-# install.packages('maps')
-# install.packages('maptools')
-# install.packages('mAr')
-# install.packages('matrixStats')
-# install.packages('mclust')
-# install.packages('MEMSS')
-# install.packages('mgcv')
-# install.packages('modeest')
-# install.packages('nlme')
-# install.packages('nortest')
-# install.packages('numDeriv', dependencies=T)  ## To calculate gradient, hessian, jacobian of functions
-# install.packages('nws')
-# install.packages('PerformanceAnalytics')
-# install.packages('phangorn')
-# install.packages('plotrix')
-# install.packages('pls')
-# install.packages('plyr', dependencies=T) ## Wont install on lmcg
-# install.packages('profr') ## Wont install on lmcg
-# install.packages('psych')
-# install.packages('quantmod')
-# install.packages('quantreg')
-# install.packages('R2HTML')
-# install.packages('rbenchmark')
-# install.packages('Rcpp')
-# install.packages('reshape', dependencies=T) ## Wont install on lmcg
-# install.packages('reshape2', dependencies=T) ## Wont install on lmcg
-# install.packages('RMySQL')      
-# install.packages('robustbase')  
-# install.packages('Rsymphony')
-# install.packages('sandwich')
-# install.packages('SenSrivastava')
-# install.packages('sets', dependencies=T) ## Set operations      
-# install.packages('slam')        
-# install.packages('sos')
-# install.packages('st')
-# install.packages('stats')
-# install.packages('survey')
-# install.packages('svMisc')
-# install.packages('tseries')
-# install.packages('TTR')
-# install.packages('urca')
-# install.packages('vrtest')
-# install.packages('wle')
-# install.packages('wordcloud')
-# install.packages('WriteXLS')
-# install.packages('xlsReadWrite')
-# #xls.getshlib()
-# install.packages('XML')
-# install.packages('xtable')
-# install.packages('xts')
-# install.packages('zipcode')
-# install.packages('zoo')
-# 
-# install.packages('fSeries')
-# 
-# install.packages('bigmemory')
-# install.packages('doSNOW')
-# install.packages('snow')
-# install.packages('plyr')
-# 
-# source('http://www.rmetrics.org/Rmetrics.R')
-# install.Rmetrics()
-
 
