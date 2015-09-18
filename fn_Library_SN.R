43cf94af6ea52238a994acda525f41746c80b94b
snandi <nandi@stat.wisc.edu>
2015-09-18 18:15:56 -0500

    Added trunc_vector function

--- fn_Library_SN.R
+++ fn_Library_SN.R
@@ -53,4 +53,18 @@ rowVar <- function(Data){
 colVar <- function(Data){
   rowVar(t(Data))
 }
+##################################################################
+
 ################################################################## 
+## Truncate user-defined percentage from both sides of vector
+##################################################################
+trunc_vector <- function(Vector, pct_trunc_left = 10, pct_trunc_right = 10){
+  ## truncates 10% from left and 10% from right by default
+  Vector <- as.vector(Vector)
+  N <- length(Vector)
+  LeftIndex <- N*pct_trunc_left/100
+  RightIndex <- N - N*pct_trunc_right/100
+  
+  Vector.Trunc <- Vector[LeftIndex : RightIndex]
+  return(Vector.Trunc)  
+}
